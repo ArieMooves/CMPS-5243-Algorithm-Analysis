5532a3b5727a8214dae09493f66b0f60409e581e
Aretha Fontaine <arethafontaine21@gmail.com>
2026-03-27 15:44:50 -0500

    Update counters.hpp

--- Assignments/P02/src/include/counters.hpp
+++ Assignments/P02/src/include/counters.hpp
@@ -1,16 +1,16 @@
-// ------------------------------
-// Counters: what Python expects
-// ------------------------------
-
 #pragma once
-#include <iostream>
-#include "json.hpp"
-#include <fstream>
 
+#include <iostream>
+#include <fstream>
+#include <stdexcept>
+#include "json.hpp"
 
 using namespace std;
 using json = nlohmann::json;
 
+// ------------------------------
+// Counters: tracks all metrics
+// ------------------------------
 struct Counters {
     long long comparisons = 0;
     long long structural_ops = 0;
@@ -18,25 +18,42 @@ struct Counters {
     long long deletes = 0;
     long long lookups = 0;
     long long resize_events = 0;
+    long long shifts_relinks = 0;   
 
+    // Pretty print 
     friend std::ostream& operator<<(std::ostream& os, const Counters &o){
-        return os << "Comp:" << o.comparisons
-                  << " Ops:" << o.structural_ops
-                  << " Inserts: " << o.inserts << std::endl;
+        return os << "Comp: " << o.comparisons
+                  << " | Ops: " << o.structural_ops
+                  << " | Inserts: " << o.inserts
+                  << " | Deletes: " << o.deletes
+                  << " | Lookups: " << o.lookups
+                  << " | Resizes: " << o.resize_events
+                  << " | Shifts/Relinks: " << o.shifts_relinks
+                  << std::endl;
     }
 
-    void saveCounters(const std::string& filename = "counters.json", bool asDict=true) const {
+    // Save to JSON file
+    void saveCounters(const std::string& filename = "counters.json", bool asDict = true) const {
         json j;
 
-        if(asDict){
+        if (asDict) {
             j["comparisons"] = comparisons;
             j["structural_ops"] = structural_ops;
             j["inserts"] = inserts;
             j["deletes"] = deletes;
             j["lookups"] = lookups;
             j["resize_events"] = resize_events;
-        }else{
-            j = {comparisons,structural_ops,inserts,deletes, lookups,resize_events};
+            j["shifts_relinks"] = shifts_relinks;  
+        } else {
+            j = {
+                comparisons,
+                structural_ops,
+                inserts,
+                deletes,
+                lookups,
+                resize_events,
+                shifts_relinks   
+            };
         }
 
         std::ofstream file(filename);
@@ -44,6 +61,6 @@ struct Counters {
             throw std::runtime_error("Failed to open file for writing");
         }
 
-        file << j.dump(4); // pretty print with 4-space indent
+        file << j.dump(4); // pretty print
     }
 };
